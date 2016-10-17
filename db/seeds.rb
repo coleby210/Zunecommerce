d9dd4e00931ecd70c124f1c00ae389c9b94bd0c8
Coleby Kent <coleby210@gmail.com>
2016-10-17 16:02:42 -0700

    Install devise and setup users in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@admin.com", first_name: "admin", last_name: "admin", password: "123123123", admin: true)
