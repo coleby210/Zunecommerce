6d0700b155d11ada10b3ed4121e22637c15eb85e
Coleby Kent <coleby210@gmail.com>
2016-11-04 20:48:43 -0700

    Setup promos to properly render on the homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
     settings = Setting.first
     @home = settings.homepage
     @sliders = @home.sliders
+    @promos = @home.promos.where.not(id: 1)
+    @first_promo = @home.promos.first
 
     @options = ["sliders", "promos", "featured_products", "banners", "testimonials", "newsletter"]
     render "/index"
