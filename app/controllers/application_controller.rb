b9caa43a17e8f7b828f9330af1d8e7ed48503a9b
Coleby Kent <coleby210@gmail.com>
2016-11-04 18:59:53 -0700

    Properly render the sliders on the homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    settings = Setting.first
+    @home = settings.homepage
+    @sliders = @home.sliders
     render "/index"
   end
 end
